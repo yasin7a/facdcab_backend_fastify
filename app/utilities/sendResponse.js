64abf72b8780e75aaaf91831681ddd0121cc1940
yasin7a <arafath7yasin@gmail.com>
2026-01-06 19:33:11 +0600

    dd

--- app/utilities/sendResponse.js
+++ app/utilities/sendResponse.js
@@ -4,8 +4,10 @@ const sendResponse = (reply, code, message, data) => {
   let responseObject = {};
   responseObject.success = true;
   responseObject.code = code || httpStatus.OK;
+  responseObject.endpoint = reply.request.url;
   responseObject.message = message || "data received successfully";
   responseObject.data = data;
+
   return reply.code(responseObject.code).send(responseObject);
 };
 
